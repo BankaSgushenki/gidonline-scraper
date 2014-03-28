69c0aa7d1d8b360c78ae82ea17bc6b6abdff5f68
Kostya Sergeev <kostyasergeev93@gmail.com>
2014-03-28 21:53:02 +0300

    add html loader

---
+++ gidonline-scraper/GetHTML.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace gidonline_scraper
+{
+     class Request
+    {
+        static String Response = "It's still empty";
+
+        public static async Task<string> getInfo(string URL)
+        {
+            HttpClient client = new HttpClient();
+            HttpResponseMessage response = new HttpResponseMessage();
+            response = await client.GetAsync(URL);
+            response.EnsureSuccessStatusCode();
+            Response = await response.Content.ReadAsStringAsync();
+            return Response;
+        }
+    }
+}
+}
